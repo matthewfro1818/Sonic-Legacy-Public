075b27c17f34af645ddce1cae7a2db2e3b8df6ee
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-08-10 17:05:34 -0400

    Add files via upload

---
+++ source/mobile/MobileOptionsState.hx
@@ -0,0 +1,147 @@
+package mobile;
+
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.utils.Assets;
+import flixel.FlxSubState;
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.util.FlxSave;
+import haxe.Json;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxTimer;
+import flixel.input.keyboard.FlxKey;
+import flixel.graphics.FlxGraphic;
+import meta.data.Controls;
+import meta.data.*;
+import meta.states.*;
+import meta.states.substate.*;
+import gameObjects.*;
+import meta.states.desktop.DesktopOptionsState;
+
+using StringTools;
+
+class MobileOptionsState extends MusicBeatState
+{
+	var options:Array<String> = ['Android Controls Settings', 'Controls'];
+	private var grpOptions:FlxTypedGroup<Alphabet>;
+	private static var curSelected:Int = 0;
+	public static var menuBG:FlxSprite;
+
+	function openSelectedSubstate(label:String) {
+		switch(label) {
+			case 'Android Controls Settings':
+		    #if mobile
+        removeVirtualPad();
+        #end
+				openSubState(new mobile.AndroidSettingsSubState());
+			case 'Controls':
+		    #if mobile
+        removeVirtualPad();
+        #end
+				openSubState(new mobile.MobileControlsSubState());
+		}
+	}
+
+	var selectorLeft:Alphabet;
+	var selectorRight:Alphabet;
+
+	override function create() {
+
+		//FlxG.sound.playMusic(Paths.music('options'));
+		MainMenuState.fromTitle = false;
+
+		DiscordHandler.changePresence("Options Menu", null);
+
+		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
+		bg.color = 0xFFea71fd;
+		bg.updateHitbox();
+
+		bg.screenCenter();
+		bg.antialiasing = ClientPrefs.data.antialiasing;
+		add(bg);
+
+		grpOptions = new FlxTypedGroup<Alphabet>();
+		add(grpOptions);
+
+		for (i in 0...options.length)
+		{
+			var optionText:Alphabet = new Alphabet(0, 0, options[i], true, false);
+			optionText.screenCenter();
+			optionText.y += (100 * (i - (options.length / 2))) + 50;
+			grpOptions.add(optionText);
+		}
+
+		selectorLeft = new Alphabet(0, 0, '>', true, false);
+		add(selectorLeft);
+		selectorRight = new Alphabet(0, 0, '<', true, false);
+		add(selectorRight);
+
+		changeSelection();
+		ClientPrefs.saveSettings();
+
+   #if mobile
+   addVirtualPad(UP_DOWN, A_B);
+   #end
+
+		super.create();
+	}
+
+	override function closeSubState() {
+		super.closeSubState();
+		ClientPrefs.saveSettings();
+	}
+
+	override function update(elapsed:Float) {
+		super.update(elapsed);
+
+		if (controls.UI_UP_P) {
+			changeSelection(-1);
+		}
+		if (controls.UI_DOWN_P) {
+			changeSelection(1);
+		}
+
+		if (controls.BACK) {
+			FlxG.sound.play(Paths.sound('cancelMenu'));
+		  MusicBeatState.switchState(new DesktopOptionsState());
+		}
+
+		if (controls.ACCEPT) {
+			openSelectedSubstate(options[curSelected]);
+		}
+	}
+
+	function changeSelection(change:Int = 0) {
+		curSelected += change;
+		if (curSelected < 0)
+			curSelected = options.length - 1;
+		if (curSelected >= options.length)
+			curSelected = 0;
+
+		var bullShit:Int = 0;
+
+		for (item in grpOptions.members) {
+			item.targetY = bullShit - curSelected;
+			bullShit++;
+
+			item.alpha = 0.6;
+			if (item.targetY == 0) {
+				item.alpha = 1;
+				selectorLeft.x = item.x - 63;
+				selectorLeft.y = item.y;
+				selectorRight.x = item.x + item.width + 15;
+				selectorRight.y = item.y;
+			}
+		}
+		FlxG.sound.play(Paths.sound('scrollMenu'));
+	}
+}
